6c4991f47a71869f04b4dc72c3636861a0aeaf43
nurul286 <Nurulalam@matchps.com>
2022-07-18 20:35:02 +0600

    Added navigation and routes in App.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
+import { ContextProvider } from './contexts/ContextProvider';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+     <ContextProvider>
       <App />
+     </ContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );

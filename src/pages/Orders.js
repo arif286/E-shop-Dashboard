b0173680562a1ca2b52226faa3d520758a3808de
nurul286 <Nurulalam@matchps.com>
2022-07-20 15:31:49 +0600

    added orders API in orders page

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,16 +1,32 @@
 import { ColumnDirective, ColumnsDirective, ContextMenu, Edit, ExcelExport, Filter, GridComponent, Inject, Page, PdfExport, Resize, Sort } from '@syncfusion/ej2-react-grids';
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Header } from '../components';
-import { contextMenuItems, ordersData, ordersGrid } from '../data/dummy';
+import { contextMenuItems, ordersGrid } from '../data/dummy';
 
 const Orders = () => {
   const editing = { allowDeleting: true, allowEditing: true };
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+      let isMounted = true;
+        axios.get(`${process.env.REACT_APP_API_URL}/api/orders`)
+        .then((res) => {
+          if (isMounted) {
+            setOrders(res.data.data);
+          }
+        })
+        .catch((err) => { console.log(err);})
+        .finally(() => { isMounted = false })
+    }, [])
+    console.log('orders',orders);
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
       <GridComponent
         id="gridcomp"
-        dataSource={ordersData}
+        dataSource={orders}
         allowPaging
         allowSorting
         allowExcelExport
